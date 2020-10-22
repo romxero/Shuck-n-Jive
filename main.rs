50c05b243b2cd32dbbbe24098e121e81f63218f6
_RC_ <randy.white.1986@gmail.com>
2020-10-21 23:53:27 -0700

    test

---
+++ main.rs
@@ -0,0 +1,38 @@
+/* This module is used to create the metadata tags associated with the blogg
+ * 
+ * 
+ * 
+ * 
+ */
+
+//~ use std::env;
+//~ use std::fs;
+//~ use std::mem;
+
+
+fn main() {
+
+
+
+
+
+let mut testVec : Vec<String> = Vec::new(); // testing out the vector things
+
+testVec.push("<meta name='keywords' content=''".to_string()); 
+testVec.push("/>".to_string()); 
+
+for x in testVec.iter() {
+        print!("{}", x);
+    }
+println!(""); //extra whitespace
+//  
+//
+//
+    //~ println!("Hello, world!");
+//
+//
+//
+
+}
+
+
