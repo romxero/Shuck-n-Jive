00dfbcb81257371f0f7e8af01de342da68fd7b6f
Randy White <romxero@hotmail.com>
2020-10-26 02:29:16 -0700

    added command line arguments

--- main.rs
+++ main.rs
@@ -5,11 +5,13 @@
  * 
  */
 
+//I use camel case, so turn off snake_case compiler messages
+#![allow(non_snake_case)]
 
 //some standard libraries
 use std::env;
 use std::fs;
-//~ use std::mem;
+use std::mem;
 
 
 //the yaml deps
@@ -18,8 +20,29 @@ use std::io::BufReader;
 use yaml::constructor::*;
 
 
+/*
+ * //these below are ripped from jekyll, trying to follow these attributes 
+  compose               
+  docs                  
+  import                
+  build, b              Build your site
+  clean                 Clean the site (removes site output and metadata file) without building.
+  doctor, hyde          Search site and print specific deprecation warnings
+  help                  Show the help message, optionally for a given subcommand.
+  new                   Creates a new Jekyll site scaffold in PATH
+  new-theme             Creates a new Jekyll theme scaffold
+  serve, server, s      Serve your site locally
+ 
+ * 
+ * 
+ * 
+ */
+
 
 fn main() {
+// taken from manual => https://doc.rust-lang.org/book/ch12-01-accepting-command-line-arguments.html
+let commandLineArgs: Vec<String> = env::args().collect();
+    println!("{:?}", commandLineArgs);
 
 
 
@@ -32,6 +55,8 @@ testVec.push("/>".to_string());
 for x in testVec.iter() {
         print!("{}", x);
     }
+
+
 println!(""); //extra whitespace
 //  
 //
